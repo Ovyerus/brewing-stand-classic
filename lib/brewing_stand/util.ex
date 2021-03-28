8c914f4e123734bcf008fa496a311e072c8be6f5
Ovyerus <iamovyerus@gmail.com>
2021-03-28 15:20:52 +1100

    feat: add `from_short` helperr

--- lib/brewing_stand/util.ex
+++ lib/brewing_stand/util.ex
@@ -21,17 +21,17 @@ defmodule BrewingStand.Util do
     end
   end
 
-  # @spec next_short(packet()) :: {:ok, short(), packet()} | length_error()
-  # def next_short(data) when is_list(data) do
-  #   if length(data) >= 2 do
-  #     {short, rest} = Enum.split(data, 2)
-  #     short = short
+  @spec next_short(packet()) :: {:ok, short(), packet()} | length_error()
+  def next_short(data) when is_list(data) do
+    if length(data) >= 2 do
+      {short, rest} = Enum.split(data, 2)
+      int = from_short(short)
 
-  #     {:ok, short, rest}
-  #   else
-  #     {:error, :too_short}
-  #   end
-  # end
+      {:ok, int, rest}
+    else
+      {:error, :too_short}
+    end
+  end
 
   # TODO: probably need to split up larger chars into individual bytes, dunno
   @spec pad_string(charlist()) :: charlist()
@@ -47,16 +47,24 @@ defmodule BrewingStand.Util do
 
   @spec to_short(short()) :: list(byte())
   def to_short(int) when is_integer(int) and int > -32768 and int <= 32767 do
-    # Split the int up into bit digits, and pad up to 15,so that we can prepend the sign
-    bits = int |> abs |> Integer.digits(2) |> :string.pad(15, :leading, 0) |> List.flatten()
+    # Split the int up into bit digits, and pad up to 15, so that we can prepend the sign
+    bits = int |> abs() |> to_bits() |> :string.pad(15, :leading, 0) |> List.flatten()
     sign = if int < 0, do: 1, else: 0
     bits = [sign | bits]
     # Divide into bytes
     {first, second} = Enum.split(bits, 8)
 
-    [Integer.undigits(first, 2), Integer.undigits(second, 2)]
+    [from_bits(first), from_bits(second)]
   end
 
-  # def from_short([first, second]) do
-  # end
+  def from_short([first, second]) do
+    bits = to_bits(first) ++ to_bits(second)
+    [sign | bits] = bits |> :string.pad(16, :leading, 0) |> List.flatten()
+    bits = if sign == 1, do: Enum.map(bits, fn x -> x * -1 end), else: bits
+
+    from_bits(bits)
+  end
+
+  defp to_bits(int), do: Integer.digits(int, 2)
+  defp from_bits(bits), do: Integer.undigits(bits, 2)
 end
